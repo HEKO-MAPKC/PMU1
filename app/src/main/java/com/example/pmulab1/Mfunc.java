e70c58921bc2dbc3ece7d72e2a5e21e204e15405
Michael <michaelkim@ya.ru>
2022-09-08 23:37:37 +0700

    Add javadoc

--- app/src/main/java/com/example/pmulab1/Mfunc.java
+++ app/src/main/java/com/example/pmulab1/Mfunc.java
@@ -1,6 +1,14 @@
 package com.example.pmulab1;
 
+/**
+ * Class find min, max
+ */
 public class Mfunc {
+    /**
+     * Func find max
+     * @param arr input
+     * @return max
+     */
     public int getMax(int[] arr){
         int maxV = arr[0];
         for(int i=1;i < arr.length;i++){
@@ -10,6 +18,12 @@ public class Mfunc {
         }
         return maxV;
     }
+
+    /**
+     * Func find min
+     * @param arr input
+     * @return min
+     */
     public int getMin(int[] arr){
         int minV = arr[0];
         for(int i=1;i<arr.length;i++){
