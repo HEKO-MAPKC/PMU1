61ab2a195dd3d11f2ed6600ca0e5f779713e90a7
Michael <michaelkim@ya.ru>
2022-09-08 20:46:19 +0700

    Add func

--- app/src/main/java/com/example/pmulab1/Mfunc.java
+++ app/src/main/java/com/example/pmulab1/Mfunc.java
@@ -1,5 +1,22 @@
 package com.example.pmulab1;
 
 public class Mfunc {
-   
+    public int getMax(int[] arr){
+        int maxV = arr[0];
+        for(int i=1;i < arr.length;i++){
+            if(arr[i] > maxV){
+                maxV = arr[i];
+            }
+        }
+        return maxV;
+    }
+    public int getMin(int[] arr){
+        int minV = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] < minV){
+                minV = arr[i];
+            }
+        }
+        return minV;
+    }
 }
