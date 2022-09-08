3e765001bd7e89caa3cb6d6aa46b6e471871708c
Michael <michaelkim@ya.ru>
2022-09-08 20:50:21 +0700

    Add unit test

--- app/src/test/java/com/example/pmulab1/ExampleUnitTest.java
+++ app/src/test/java/com/example/pmulab1/ExampleUnitTest.java
@@ -13,6 +13,24 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         Mfunc mfunc = new Mfunc();
+
+        int arr[] = new int[]{10, 11, 1};
+        assertEquals(mfunc.getMin(arr),1);
+        arr = new int[]{10, 11, 1,-1,-100};
+        assertEquals(mfunc.getMin(arr),-100);
+        arr = new int[]{10};
+        assertEquals(mfunc.getMin(arr),10);
+        arr = new int[]{10, 11, 112,123,10};
+        assertEquals(mfunc.getMin(arr),10);
+
+        arr = new int[]{10, 11, 1};
+        assertEquals(mfunc.getMax(arr),11);
+        arr = new int[]{10, 11, 1,-1,-100};
+        assertEquals(mfunc.getMax(arr),11);
+        arr = new int[]{10};
+        assertEquals(mfunc.getMax(arr),10);
+        arr = new int[]{10, 11, 112,123,10};
+        assertEquals(mfunc.getMax(arr),123);
     }
 
 }
